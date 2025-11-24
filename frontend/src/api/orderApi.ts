7090cadeab0f80c5f3efd039f085e798b86ccf47
Trung Nguyen <trungnguyen1221999@gmail.com>
2025-11-24 14:45:39 +0200

    Update orderApi.ts

--- frontend/src/api/orderApi.ts
+++ frontend/src/api/orderApi.ts
@@ -1,29 +1,28 @@
 import api from "./api";
 
-const BASE_URL = '/order';
 
 export const createAbandonOrderApi = async (orderData: any) => {
-  const res = await api.post(BASE_URL + '/abandon', orderData);
+  const res = await api.post('/order/abandon', orderData);
   return res.data;
 };
 
 export const completeAbandonOrderApi = async (orderData: any) => {
-  const res = await api.put(BASE_URL + '/abandon/complete', orderData);
+  const res = await api.put('/order/abandon/complete', orderData);
   return res.data;
 };
 
 
 export const createOrderApi = async (orderData: any) => {
-  const res = await api.post(BASE_URL, orderData);
+  const res = await api.post('/order', orderData);
   return res.data;
 };
 
 export const getOrderApi = async (id: string) => {
-  const res = await api.get(`${BASE_URL}/${id}`);
+  const res = await api.get(`/order/${id}`);
   return res.data;
 };
 
 export const getOrdersApi = async (params?: { userId?: string; guestId?: string }) => {
-  const res = await api.get(BASE_URL, { params });
+  const res = await api.get('/order', { params });
   return res.data;
 };
