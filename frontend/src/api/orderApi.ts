a7bca641e9eabf8eec621480d842ef1623f6853e
Trung Nguyen <trungnguyen1221999@gmail.com>
2025-11-24 14:41:45 +0200

    Update orderApi.ts

--- frontend/src/api/orderApi.ts
+++ frontend/src/api/orderApi.ts
@@ -1,28 +1,29 @@
-import axios from 'axios';
-const BASE_URL = '/api/order';
+import api from "./api";
+
+const BASE_URL = '/order';
 
 export const createAbandonOrderApi = async (orderData: any) => {
-  const res = await axios.post(BASE_URL + '/abandon', orderData);
+  const res = await api.post(BASE_URL + '/abandon', orderData);
   return res.data;
 };
 
 export const completeAbandonOrderApi = async (orderData: any) => {
-  const res = await axios.put(BASE_URL + '/abandon/complete', orderData);
+  const res = await api.put(BASE_URL + '/abandon/complete', orderData);
   return res.data;
 };
 
 
 export const createOrderApi = async (orderData: any) => {
-  const res = await axios.post(BASE_URL, orderData);
+  const res = await api.post(BASE_URL, orderData);
   return res.data;
 };
 
 export const getOrderApi = async (id: string) => {
-  const res = await axios.get(`${BASE_URL}/${id}`);
+  const res = await api.get(`${BASE_URL}/${id}`);
   return res.data;
 };
 
 export const getOrdersApi = async (params?: { userId?: string; guestId?: string }) => {
-  const res = await axios.get(BASE_URL, { params });
+  const res = await api.get(BASE_URL, { params });
   return res.data;
 };
